5656c225dfa1a30292405875ac44ab247bc6f179
Mayur Hapani <hapanimayuur@gmail.com>
2024-05-22 00:01:20 +0530

    user creation done

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require("express");
-const db = require("./config/database");
 const app = express();
 port = 8001;
 
+const db = require("./config/database");
+const userModel = require("./models/user");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(8002, (err) => {
+app.post("/addUser", async (req, res) => {
+  console.log(req.body);
+
+  const { username, name, email, age, password } = req.body;
+
+  try {
+    await userModel.create({ username, name, email, age, password });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, (err) => {
   if (!err) console.log("Server is running on http://localhost:" + port);
 });
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://hapanimayur:Love1224@cluster0.iyurgqh.mongodb.net/MiniProjectBlog");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  age: Number,
+  password: String,
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
